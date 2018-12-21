477a8a160e5aec2f7f3ec4a319b4c4a2286fb559
Shi Hao <shihao06@gmail.com>
2018-12-21 15:14:32 +0800

    Add files via upload

--- 0918s.py
+++ 0918s.py
@@ -80,7 +80,7 @@ def query_cycle():
 		time_hour = int(time.strftime('%H',time.localtime(time.time())))
 		time_day = int(time.strftime('%w',time.localtime(time.time())))
 		if time_day == 0 or time_day == 6:
-			time_delay = 43200
+			time_delay = 28800
 		elif time_hour == 12 or time_hour == 18:
 			time_delay = 600
 		else:
