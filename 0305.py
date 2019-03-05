12e2290a9248110feef97711409063981c7f0680
Shi Hao <shihao06@gmail.com>
2019-03-05 16:54:53 +0800

    Update 0305.py

--- 0305.py
+++ 0305.py
@@ -1,6 +1,6 @@
 import telegram
 bot = telegram.Bot(token='637785666:AAHRW-gz-CeKkSGbP_xKubcau0dO28ffBYc')
 print bot.getMe()
-bot.send_message(chat_id='FindSthToplay',
+bot.send_message(chat_id='AAAAAFFzP2u9YkNKVbDC2g',
     text='<a href="http://cn.bing.com">bing</a>.', 
     parse_mode=telegram.ParseMode.HTML)
