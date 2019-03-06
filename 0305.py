0169a1118e8a4395f009804826d23a89d8d1cb37
Shi Hao <shihao06@gmail.com>
2019-03-06 15:03:47 +0800

    Add files via upload

--- 0305.py
+++ 0305.py
@@ -1,4 +1,4 @@
-import telegram
-bot = telegram.Bot(token='637785666:AAHRW-gz-CeKkSGbP_xKubcau0dO28ffBYc')
-print bot.getMe()
-bot.send_message('FindSthToPlay', '<a href="http://cn.bing.com">bing</a>.')
+import request
+
+url = "https://api.telegram.org/637785666:AAHRW-gz-CeKkSGbP_xKubcau0dO28ffBYc/sendMessage?chat_id=@FindSthToEat&text=message"
+requests.post(url)
