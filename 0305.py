98040c9c04a448fefad2ed28e61fc86d09780b9d
Shi Hao <shihao06@gmail.com>
2019-03-06 14:44:49 +0800

    Update 0305.py

--- 0305.py
+++ 0305.py
@@ -1,5 +1,4 @@
 import telegram
 bot = telegram.Bot(token='637785666:AAHRW-gz-CeKkSGbP_xKubcau0dO28ffBYc')
 print bot.getMe()
-bot.send_message('FindSthToPlay',
-    '<a href="http://cn.bing.com">bing</a>.')
+bot.send_message('FindSthToPlay', '<a href="http://cn.bing.com">bing</a>.')
