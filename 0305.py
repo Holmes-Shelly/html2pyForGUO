144a8d1e1525b50bf3c0c7ab678a502ba87fd5e6
Shi Hao <shihao06@gmail.com>
2019-03-07 14:10:22 +0800

    Add files via upload

--- 0305.py
+++ 0305.py
@@ -1,4 +1,4 @@
 import requests
 
 url = "https://api.telegram.org/637785666:AAHRW-gz-CeKkSGbP_xKubcau0dO28ffBYc/sendMessage?chat_id=@FindSthToPlay&text=message"
-requests.post(url)
+print requests.post(url).content
