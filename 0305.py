095a862ae68fc64a1d26c552bf208f6ecd980c46
Shi Hao <shihao06@gmail.com>
2019-03-14 13:59:43 +0800

    Delete 0305.py

--- 0305.py
+++
@@ -1,9 +0,0 @@
-import requests
-TOKEN = "637785666:AAHRW-gz-CeKkSGbP_xKubcau0dO28ffBYc"
-url = "https://api.telegram.org/bot{}/".format(TOKEN)
-# print requests.get(url +"getme").content
-# print requests.get(url + "getUpdates").content
-def send_message(msg):
-	status = requests.get(url + "sendMessage?chat_id=-1001366507371&text={}".format(msg)).content
-	return status
-print send_message("8 updated, 2 important")
