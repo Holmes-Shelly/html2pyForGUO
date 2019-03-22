cf2242f142ecde53e1277bd1c13ffaed10c528bc
Shi Hao <shihao06@gmail.com>
2019-03-22 10:14:26 +0800

    Add files via upload

--- 0918s.py
+++ 0918s.py
@@ -12,35 +12,33 @@ key_word = u'基金募集申请'
 
 # main function
 def html_query():
-	# 获取网页内容并分析
 	html_response = requests.get(url).content.decode('utf-8')
 	this_query = html_analyze(html_response)
 	
-	# 与上次查询进行对比，是否有更新
 	global last_query
 	diff_tuple = tuple(set(this_query).difference(set(last_query)))
 	if len(diff_tuple) and len(last_query):
 		print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), len(diff_tuple), "updated."
 		key_query = find_key(diff_tuple)
 		print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), len(key_query), "important."
+		send_tg(diff_tuple, key_query)
 		send_email(diff_tuple, key_query)
 	last_query = this_query
 	return
 
-# 对网页内容进行正则分析的函数，返回一个长度为10的，包含10个title字符串
 def html_analyze(content):
 	res = r'<.*?titleshow.*?>(.*?)<.*?>'
 	ans = re.findall(res, content, re.I|re.S|re.M)
 	return tuple(ans)
 	
 def send_email(html_tuple, msg_tuple):
-	# 第三方 SMTP 服务
-	mail_host="smtp.163.com"  #设置服务器
-	mail_user="shihao1024@163.com"   #用户名
-	mail_pass="shihao1992"   #口令
+	
+	mail_host="smtp.163.com"  
+	mail_user="shihao1024@163.com"   
+	mail_pass="shihao1992"  
 
 	sender = 'shihao1024@163.com'
-	receivers = ['marygmd123@163.com', 'shihao1024@163.com']  # 接收邮件
+	receivers = ['marygmd123@163.com', 'shihao1024@163.com']  
 	subject = str(len(html_tuple))+" updated, "+str(len(msg_tuple))+" important."
 	email_content = ''
 	content_row = 0
@@ -58,10 +56,8 @@ def send_email(html_tuple, msg_tuple):
 		smtpObj.connect(mail_host, 465)
 		smtpObj.login(mail_user,mail_pass)
 		smtpObj.sendmail(sender, receivers, message.as_string())
-		# print "send successfully"
 	except smtplib.SMTPException:
 		print "send unsuccessfully"
-		send_tg(html_tuple, msg_tuple)
 	return
 	
 def send_tg(html_tuple, msg_tuple):
@@ -71,12 +67,10 @@ def send_tg(html_tuple, msg_tuple):
 
 	try:
 		requests.get(url + "sendMessage?chat_id=-1001366507371&text={}".format(content.encode('utf-8')))
-		print "tg send successfully"
 	except:
 		print "tg send unsuccessfully"
 	return
 	
-# 分析是否含有“基金募集”
 def find_key(diff_tuple):
 	key_query = []
 	for query_content in diff_tuple:
@@ -85,14 +79,12 @@ def find_key(diff_tuple):
 			# print 'find', type(query_content)
 	return tuple(key_query)
 	
-# 网页查询函数
 def query_cycle():
 	while(1):
-		# 间隔时间设置
 		time_hour = int(time.strftime('%H',time.localtime(time.time())))
 		time_day = int(time.strftime('%w',time.localtime(time.time())))
-		if time_hour == 21:
-			time_delay = 43200
+		if time_hour == 22:
+			time_delay = 36000
 		elif time_day == 0 or time_day == 6:
 			time_delay = 3600
 		elif time_hour == 12 or time_hour == 18:
@@ -100,7 +92,6 @@ def query_cycle():
 		else:
 			time_delay = 3600
 
-		# 查询
 		try:
 			print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 			html_query()
@@ -110,5 +101,4 @@ def query_cycle():
 		time.sleep(time_delay)
 	return
 
-# 开始查询	
 query_cycle()
